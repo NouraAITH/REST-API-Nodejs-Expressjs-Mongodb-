8006a46790b99100f5bfa075720ab52ee058ab26
Noura AIT <noura.aitlhousse@gmail.com>
2020-09-21 19:27:07 +0100

    Basic Auth + Cookies

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mongoose=require('mongoose');
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db)=> {console.log('Connected successfuly')}, (err)=> {console.log(err)});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
@@ -19,7 +27,63 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
+
+function auth(req,res,next) {
+   console.log(req.signedCookies);
+
+   if(!req.signedCookies.user){
+      
+    var authHeader= req.headers.authorization;
+   if(!authHeader){
+       var err= new Error('You are not authonticated!!');
+       res.setHeader('WWW-Authenticate', 'Basic');
+       err.status=401;
+       return next(err);
+
+   }
+
+           var auth=new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+           var user= auth[0];
+           var pass=auth[1];
+           if (user === 'admin' && pass === 'password') {
+             res.cookie('user', 'admin', {signed: true});
+            next(); // authorized
+        }
+
+        else{
+          var err= new Error('You are not authonticated!!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+               err.status=401;
+             return next(err);
+
+        }
+
+
+   }
+
+   else {
+    if (req.signedCookies.user === 'admin') {
+        next();
+    }
+    else {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+    }
+}
+
+   
+
+
+}
+app.use(auth);
+
+
+
+
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
